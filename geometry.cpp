76d2582cd86573313f1fb072c018050addb94942
CarolinaMoura <36082306+CarolinaMoura@users.noreply.github.com>
2020-10-10 17:06:28 -0300

    Update geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -13,43 +13,19 @@
 
 using namespace std ;
 
-template<class T>
-class minRmq
+struct Point
 {
-	/*
-		Considerations:
-		* it makes minimum range queries
-		* it's indexed from 0
-		* just support queries [l;r] where r < n
-	*/
 
-	int n ;
-	vector<T> _log ;
-	vector< vector<T> > dp ;
+	ll x , y ;
 
-	public:
+	Point(ll x=0, ll y=0):x(x), y(y) {}
+	
+	Point operator - (Point other) const { return Point(x-other.x, y-other.y) ; } 
+	ll operator % (Point other) const { return x*other.y - y*other.x ; }
+	ll operator *(Point other) const { return x*other.x + y*other.y ; }
+	void print() { printf("%lld %lld\n" , x , y) ; }
 
-		void initialize(vector<T> &a)
-		{
-			n = (int)(a.size() ) ;
-			_log.resize(n+1,0) ;
-
-			for(int i = 2 ; i <= n ; i ++ ) _log[i] = _log[i>>1]+1 ;
-			dp.resize(_log[n]+1, vector<T>(n,0) ) ;
-
-			for(int i = 0 ; i < n ; i++) dp[0][i] = a[i] ;
-			for(int i = 1 ; i <= _log[n] ; i++)
-				for(int j = 0 ; j+(1<<(i-1)) < n ; j++ )
-					dp[i][j] = min(dp[i-1][j], dp[i-1][j+(1<<(i-1))] ) ;
-		}
-
-		T query(int l, int r )
-		{
-			int t = _log[r-l+1] ;
-			return min(dp[t][l], dp[t][r-(1<<t)+1] ) ;
-		}
-
-};
+} ;
 
 
 int main()
