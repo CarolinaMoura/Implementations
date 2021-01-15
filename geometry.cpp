2eae8e5f36663c7d7e670dca5a27c4e103da54db
CarolinaMoura <36082306+CarolinaMoura@users.noreply.github.com>
2021-01-15 08:15:29 -0300

    Update geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -16,13 +16,29 @@ using namespace std ;
 struct Point
 {
 
-	ll x , y ;
+    ll x , y ;
 
-	Point(ll x=0, ll y=0):x(x), y(y) {}
-	
-	Point operator - (Point other) const { return Point(x-other.x, y-other.y) ; } 
-	ll operator % (Point other) const { return x*other.y - y*other.x ; }
-	ll operator *(Point other) const { return x*other.x + y*other.y ; }
-	void print() { printf("%lld %lld\n" , x , y) ; }
-	void read() { scanf("%lld %lld", &x, &y) ; }
+    Point(ll x=0, ll y=0):x(x), y(y) {}
+    
+    Point operator - (Point other) const { return Point(x-other.x, y-other.y) ; } 
+    ll operator % (Point other) const { return x*other.y - y*other.x ; }
+    ll operator *(Point other) const { return x*other.x + y*other.y ; }
+    void print() { printf("%lld %lld\n" , x , y) ; }
+    void read() { scanf("%lld %lld", &x, &y) ; }
+    int quadrant()
+    {
+        if(y >= 0 && x > 0) return 1 ;
+        if( y > 0 && x <= 0 ) return 2 ;
+        if( y <= 0 && x < 0 ) return 3 ;
+        if( y < 0 && x <= 0 ) return 4 ;
+        return 5 ; //origin
+    }
 } ;
+
+//To use this, the origin shall never be a valid point (or it'll end up as the last element)
+bool byPolar(Point a , Point b)
+{
+    if(a.quadrant() != b.quadrant() ) return a.quadrant() < b.quadrant() ;
+    if( a%b == 0 ) return (b-a)*( Point(0,0)-a ) < 0;
+    return a%b > 0 ;
+}
