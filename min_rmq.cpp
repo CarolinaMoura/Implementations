95146fc8993859064c7fd6379351d38d20da6fe6
CarolinaMoura <36082306+CarolinaMoura@users.noreply.github.com>
2021-02-08 19:29:31 -0300

    Update min_rmq.cpp

--- min_rmq.cpp
+++ min_rmq.cpp
@@ -39,7 +39,7 @@ class minRmq
 
 			for(int i = 0 ; i < n ; i++) dp[0][i] = a[i] ;
 			for(int i = 1 ; i <= _log[n] ; i++)
-				for(int j = 0 ; j+(1<<(i-1)) < n ; j++ )
+				for(int j = 0 ; j+(1<<i)-1 < n ; j++ )
 					dp[i][j] = min(dp[i-1][j], dp[i-1][j+(1<<(i-1))] ) ;
 		}
 
